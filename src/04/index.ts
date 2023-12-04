73bc34c2353c7b9f4ebc305c32ac277dc34c004d
Matt Mapplebeck <matt.mapplebeck@shopify.com>
2023-12-04 09:18:05 +0100

    Add Day 4 Part 2

--- src/04/index.ts
+++ src/04/index.ts
@@ -3,7 +3,7 @@ import { join } from "path";
 
 import { getInput } from "../utils";
 
-const INPUT = getInput(join(__dirname, "example-input.txt"));
+const INPUT = getInput(join(__dirname, "input.txt"));
 
 interface Card {
   numbers: number[];
@@ -31,13 +31,19 @@ function getCards(input: string[]): Card[] {
   });
 }
 
-function scoreCard(card: Card): number {
+function getMatchCount(card: Card): number {
   const winningNumbers = new Set(card.winningNumbers);
 
   const matchCount = card.numbers.reduce(
     (acc, number) => (winningNumbers.has(number) ? acc + 1 : acc),
     0
   );
+
+  return matchCount;
+}
+
+function scoreCard(card: Card): number {
+  const matchCount = getMatchCount(card);
 
   return matchCount ? 2 ** (matchCount - 1) : 0;
 }
@@ -46,4 +52,35 @@ function getPart1(input: string[]) {
   return sum(getCards(input).map(scoreCard));
 }
 
-console.log(getPart1(INPUT));
+function getMemoKey(start: number, end: number) {
+  return `${start}_${end}`;
+}
+
+function countCards(
+  cards: Card[],
+  start = 0,
+  end = cards.length,
+  memo: Record<string, number> = {}
+): number {
+  const memoKey = getMemoKey(start, end);
+
+  if (memo[memoKey] === undefined) {
+    let sum = 0;
+
+    for (let c = start; c < end; c++) {
+      const matchCount = getMatchCount(cards[c]);
+
+      sum += 1 + countCards(cards, c + 1, c + 1 + matchCount, memo);
+    }
+
+    memo[memoKey] = sum;
+  }
+
+  return memo[memoKey];
+}
+
+function getPart2(input: string[]): number {
+  return countCards(getCards(input));
+}
+
+console.log(getPart1(INPUT), getPart2(INPUT));
