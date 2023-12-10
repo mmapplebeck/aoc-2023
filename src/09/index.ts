c6302a230b3b54be7e00fcf33cdb4d7697d5a99e
Matt Mapplebeck <matt.mapplebeck@shopify.com>
2023-12-10 09:46:04 +0100

    Add Day 9 Part 1

---
+++ src/09/index.ts
@@ -0,0 +1,57 @@
+import sum from "lodash/sum";
+import { join } from "path";
+
+import { getInput } from "../utils";
+
+const INPUT = getInput(join(__dirname, "input.txt"));
+
+function extrapolate(history: number[]): number[][] {
+  let currentHistory: number[] = history;
+  let nextPrediction: number[] = [];
+  const extrapolated: number[][] = [history];
+
+  while (currentHistory.length > 1) {
+    let allZeroes = true;
+
+    for (let i = 0; i < currentHistory.length - 1; i++) {
+      const reading = currentHistory[i + 1] - currentHistory[i];
+      nextPrediction.push(reading);
+      if (reading !== 0) {
+        allZeroes = false;
+      }
+    }
+
+    if (allZeroes) {
+      break;
+    }
+
+    extrapolated.push(nextPrediction);
+    currentHistory = nextPrediction;
+    nextPrediction = [];
+  }
+
+  return extrapolated;
+}
+
+function predict(extrapolation: number[][]): number {
+  const lastRow = extrapolation[extrapolation.length - 1];
+  let prediction = lastRow[lastRow.length - 1];
+
+  for (let i = extrapolation.length - 2; i >= 0; i--) {
+    prediction += extrapolation[i][extrapolation[i].length - 1];
+  }
+
+  return prediction;
+}
+
+function getHistories(input: string[]): number[][] {
+  return input.map((line) => line.split(" ").map((str) => Number(str)));
+}
+
+function getPart1(input: string[]) {
+  return sum(
+    getHistories(input).map((history) => predict(extrapolate(history)))
+  );
+}
+
+console.log(getPart1(INPUT));
